aa3566cc893ab8e00e0a151a476e92a8d3b2363e
Calvin May <calvinkylemay.dev@gmail.com>
2025-04-22 12:30:21 -0400

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { PanGesture } from './screens/PanGesture';
+
+import { useState } from 'react';
+import type { LayoutRectangle } from 'react-native';
 
 export default function App() {
+  const [container, setContainer] = useState<null | LayoutRectangle>(null);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView>
+      <View
+        style={styles.container}
+        onLayout={({ nativeEvent: { layout } }) => {
+          setContainer(layout);
+        }}
+      >
+        {container && <PanGesture {...container} />}
+      </View>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
